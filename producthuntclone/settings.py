d2dd1b9fff551d307b063bd284a60a3728f54089
Alex Zaitsev <madeinussr91@gmail.com>
2019-06-28 18:33:53 +0300

    Moving production from postgres to mysql

--- producthuntclone/settings.py
+++ producthuntclone/settings.py
@@ -82,12 +82,8 @@ WSGI_APPLICATION = 'producthuntclone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'portfoliodb',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
